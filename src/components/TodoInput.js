5f8a3eb4fa268639ee3923bf52c9272a4c7ac94e
samkyung <rnflrntk@naver.com>
2024-07-08 17:22:26 +0900

    할 일 추가 form 이벤트, 스타일 처리

--- src/components/TodoInput.js
+++ src/components/TodoInput.js
@@ -1,20 +1,44 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../scss/TodoInput.scss';
 import { MdAdd } from 'react-icons/md';
+import cn from 'classnames';
 
 const TodoInput = () => {
+  // 입력창이 열리는 여부를 표현하는 상태값
+  const [open, setOpen] = useState(false);
+
+  // 더하기 버튼 클릭시 이벤트 발생
+  const onToggle = () => {
+    setOpen(!open);
+  };
+
   return (
     <>
-      <div className='form-wrapper'>
-        <form className='insert-form'>
-          <input
-            type='text'
-            placeholder='할 일을 입력 후, 엔터를 누루세요!'
-          />
-        </form>
-      </div>
+      {open && (
+        <div className='form-wrapper'>
+          <form className='insert-form'>
+            <input
+              type='text'
+              placeholder='할 일을 입력 후, 엔터를 누루세요!'
+            />
+          </form>
+        </div>
+      )}
 
-      <button className='insert-btn'>
+      {/* 
+        cn() : 첫번째 파라미터는 항상 유지할 default 클래스
+        두번째 파라미터는 논리 상태값
+        => 논리 상태값이 true일 경우 해당 클래스 추가
+          false일 경우 제거.
+          {클래스이름: 논리값}, 
+          클래스 이름 지정 안할 시 변수명이 클래스 이름으로 사용됨.
+      */}
+
+      <button
+        // className={!open ? 'insert-btn' : 'insert-btn open'}
+        className={cn('insert-btn', { open })}
+        onClick={onToggle}
+      >
         <MdAdd />
       </button>
     </>
