93f1cd1ef8eaed466e452a3863e9a92ac949b05e
samkyung <rnflrntk@naver.com>
2024-09-05 15:34:17 +0900

    회원가입 처리 완료 (fetch를 통한 회원가입 요청)

--- src/components/user/Join.js
+++ src/components/user/Join.js
@@ -48,9 +48,12 @@ const Join = () => {
     msg,
   }) => {
     // 입력값 세팅
-    setUserValue((oldVal) => {
-      return { ...oldVal, [key]: inputValue };
-    });
+    // 패스워드 확인 입력값은 굳이 userValue 상태로 유지할 필요가 없기 때문에
+    // 임의의 문자열 'pass'를 넘기고 있습니다. -> pass가 넘어온다면 setUserValue()를 실행하지 않습니다.
+    inputValue !== 'pass' &&
+      setUserValue((oldVal) => {
+        return { ...oldVal, [key]: inputValue };
+      });
 
     // 메세지 세팅
     setMessage((oldMsg) => {
@@ -142,6 +145,102 @@ const Join = () => {
     });
   };
 
+  // 패스워드 입력창 체인지 이벤트
+  const passwordHandler = (e) => {
+    // 패스워드가 변경됐다 -> 패스워드 확인란도 지우겠다.
+    document.getElementById('password-check').value = '';
+
+    setMessage({ ...message, passwordCheck: '' });
+    setCorrect({ ...correct, passwordCheck: '' });
+
+    const inputValue = e.target.value;
+    const pwRegex =
+      /^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,20}$/;
+
+    let msg;
+    let flag = false;
+
+    if (!inputValue) {
+      msg = '비밀번호는 필수입니다.';
+    } else if (!pwRegex.test(inputValue)) {
+      msg =
+        '8글자 이상의 영문, 숫자, 특수문자를 포함해 주세요.';
+    } else {
+      msg = '사용 가능한 비밀번호입니다.';
+      flag = true;
+    }
+
+    saveInputState({
+      key: 'password',
+      inputValue,
+      msg,
+      flag,
+    });
+  };
+
+  // 비밀번호 확인란 체인지 이벤트 핸들러
+  const pwCheckHadnler = (e) => {
+    let msg;
+    let flag = false;
+    if (!e.target.value) {
+      msg = '비밀번호 확인란은 필수입니다.';
+    } else if (userValue.password !== e.target.value) {
+      msg = '비밀번호가 일치하지 않습니다.';
+    } else {
+      msg = '비밀번호가 일치합니다.';
+      flag = true;
+    }
+    saveInputState({
+      key: 'passwordCheck',
+      inputValue: 'pass',
+      msg,
+      flag,
+    });
+  };
+
+  // 4개의 입력창이 모두 검증에 통과했는지 여부를 검사
+  const isValid = () => {
+    for (const key in correct) {
+      const flag = correct[key];
+      if (!flag) return false;
+    }
+    return true;
+  };
+
+  // 회원 가입 처리 서버 요청
+  const fetchSignupPost = () => {
+    // fetch를 사용한 회원 가입 요청
+    fetch(`${API_BASE_URL}${USER}`, {
+      method: 'POST',
+      headers: { 'content-type': 'application/json' },
+      body: JSON.stringify(userValue),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        alert(
+          `${data.userName}(${data.email})님 회원 가입에 성공했습니다.`,
+        );
+      })
+      .catch((err) => {
+        console.log('err: ', err);
+        alert(
+          '서버와의 통신이 원할하지 않습니다. 관리자에게 문의하세요.',
+        );
+      });
+  };
+
+  // 회원 가입 버튼 클릭 이벤튼 핸들러
+  const joinButtonClickHandler = (e) => {
+    e.preventDefault();
+
+    if (isValid()) {
+      // fetch를 사용한 회원 가입 요청.
+      fetchSignupPost();
+    } else {
+      alert('입력란을 다시 확인해 주세요!');
+    }
+  };
+
   return (
     <Container
       component='main'
@@ -208,8 +307,17 @@ const Join = () => {
               type='password'
               id='password'
               autoComplete='current-password'
+              onChange={passwordHandler}
             />
-            <span></span>
+            <span
+              style={
+                correct.password
+                  ? { color: 'green' }
+                  : { color: 'red' }
+              }
+            >
+              {message.password}
+            </span>
           </Grid>
 
           <Grid item xs={12}>
@@ -222,8 +330,18 @@ const Join = () => {
               type='password'
               id='password-check'
               autoComplete='check-password'
+              onChange={pwCheckHadnler}
             />
-            <span id='check-span'></span>
+            <span
+              id='check-span'
+              style={
+                correct.passwordCheck
+                  ? { color: 'green' }
+                  : { color: 'red' }
+              }
+            >
+              {message.passwordCheck}
+            </span>
           </Grid>
 
           <Grid item xs={12}>
@@ -232,6 +350,7 @@ const Join = () => {
               fullWidth
               variant='contained'
               style={{ background: '#38d9a9' }}
+              onClick={joinButtonClickHandler}
             >
               계정 생성
             </Button>
