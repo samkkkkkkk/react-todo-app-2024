57a144684302745fc31fdc2f52a412fdde04931e
samkyung <rnflrntk@naver.com>
2024-10-17 15:16:44 +0900

    axios interceptor를 활용한 access token 갱신, TodoTemplate 리팩토링

--- src/components/todo/TodoTemplate.js
+++ src/components/todo/TodoTemplate.js
@@ -17,6 +17,7 @@ import { useNavigate } from 'react-router-dom';
 import { Spinner } from 'reactstrap';
 import AuthContext from '../../utils/AuthContext';
 import axiosInstance from '../../config/axios-config';
+import handleRequest from '../../config/handleRequest';
 
 const TodoTemplate = () => {
   const API_BASE_URL = BASE + TODO;
@@ -25,13 +26,15 @@ const TodoTemplate = () => {
   // 라우팅 사용
   const redirection = useNavigate();
 
-  const { onLogout, onLogin } = useContext(AuthContext);
+  const { onLogout } = useContext(AuthContext);
 
   // todos 배열을 상태 관리
   const [todos, setTodos] = useState([]);
 
   // 로딩 상태값 관리 (처음에는 로딩이 무조건 필요하기 때문에 true -> 로딩이 끝나면 false로 진행)
   const [loading, setLoading] = useState(true);
+
+  const [token, setToken] = useState('');
 
   // TodoInput에게 todoText를 받아오는 함수
   // 자식 컴포넌트가 부모 컴포넌트에게 데이터를 전달할 때는
@@ -43,46 +46,57 @@ const TodoTemplate = () => {
       title: todoText,
     };
 
-    try {
-      const res = await axiosInstance.post(
-        API_BASE_URL,
-        newTodo,
-      );
-      if (res.status === 200) setTodos(res.data.todos);
-    } catch (error) {
-      console.log('error: ', error);
-    }
+    handleRequest(
+      () => axiosInstance.post(API_BASE_URL, newTodo),
+      (data) => setTodos(data.todos),
+      (error) => {
+        if (error.response && error.response === 401) {
+          alert(
+            '로그인 시간이 만료 되었습니다. 다시 로그인 해주세요.',
+          );
+          onLogout();
+          redirection('/login');
+        }
+      },
+    );
   };
 
   // 할 일 삭제 처리 함수
-  const removeTodo = (id) => {
-    fetch(`${API_BASE_URL}/${id}`, {
-      method: 'DELETE',
-    })
-      .then((res) => res.json())
-      .then((data) => setTodos(data.todos))
-      .catch((err) => {
-        console.log('err: ', err);
-        alert('잘못된 요청입니다.');
-      });
+  const removeTodo = async (id) => {
+    handleRequest(
+      () => axiosInstance.delete(`${API_BASE_URL}/${id}`),
+      (data) => setTodos(data.todos),
+      (error) => {
+        if (error.response && error.response === 401) {
+          alert(
+            '로그인 시간이 만료 되었습니다. 다시 로그인 해주세요.',
+          );
+          onLogout();
+          redirection('/login');
+        }
+      },
+    );
   };
 
   // 할 일 체크 처리 함수
   const checkTodo = (id, done) => {
-    fetch(API_BASE_URL, {
-      method: 'PATCH',
-
-      body: JSON.stringify({
-        id,
-        done: !done,
-      }),
-    })
-      .then((res) => res.json())
-      .then((data) => setTodos(data.todos))
-      .catch((err) => {
-        console.log('err: ', err);
-        alert('잘못된 요청입니다.');
-      });
+    handleRequest(
+      () =>
+        axiosInstance.patch(API_BASE_URL, {
+          id,
+          done: !done,
+        }),
+      (data) => setTodos(data.todos),
+      (error) => {
+        if (error.response && error.response === 401) {
+          alert(
+            '로그인 시간이 만료 되었습니다. 다시 로그인 해주세요.',
+          );
+          onLogout();
+          redirection('/login');
+        }
+      },
+    );
   };
 
   // // 체크가 안 된 할 일의 개수를 카운트 하기
@@ -91,34 +105,45 @@ const TodoTemplate = () => {
 
   // 비동기 방식 등급 승격 함수
   const fetchPromote = async () => {
-    const res = await fetch(API_USER_URL + '/promote', {
-      method: 'PUT',
-    });
-
-    if (res.status === 400) {
-      alert('이미 프리미엄 회원입니다.');
-    } else if (res.status === 200) {
-      const json = await res.json();
-      localStorage.setItem('ACCESS_TOKEN', json.token);
-      localStorage.setItem('USER_ROLE', json.role);
-    }
+    handleRequest(
+      () => axiosInstance.put(`${API_BASE_URL}/promote`),
+      (data) => {
+        localStorage.setItem('ACCESS_TOKEN', data.token);
+        localStorage.setItem('USER_ROLE', data.role);
+        setToken(data.token);
+      },
+      (error) => {
+        if (error.response && error.response === 401) {
+          alert(
+            '로그인 시간이 만료 되었습니다. 다시 로그인 해주세요.',
+          );
+          onLogout();
+          redirection('/login');
+        } else if (error.response === 400) {
+          alert('이미 프리미엄 회원입니다.');
+        }
+      },
+    );
   };
 
   useEffect(() => {
     // 페이지가 처음 렌더링 됨과 동시에 할 일 목록을 서버에 요청해서 뿌려 주겠습니다.
-
-    const fetchTodos = async () => {
-      try {
-        const res = await axiosInstance.get(API_BASE_URL);
-        if (res.status === 200) setTodos(res.data.todos);
-      } catch (error) {
-        console.log('error: ', error);
-      } finally {
+    handleRequest(
+      () => axiosInstance.get(API_BASE_URL),
+      (data) => {
+        setTodos(data.todos);
         setLoading(false);
-      }
-    };
-
-    fetchTodos();
+      },
+      (error) => {
+        if (error.response && error.response === 401) {
+          alert(
+            '로그인 시간이 만료 되었습니다. 다시 로그인 해주세요.',
+          );
+          onLogout();
+          redirection('/login');
+        }
+      },
+    );
   }, []);
 
   // 로딩이 끝난 후 보여줄 컴포넌트
--- src/config/axios-config.js
+++ src/config/axios-config.js
@@ -1,13 +1,17 @@
-// 여기서 axios 인스턴스를 생성하고,
-// interceptor 기능을 활용하여, access token이 만료 되었을 때 refresh token을 사용하여
-// 새로운 access token을 발급받는 비동기 방식으로 요청을 모듈화, (fetch는 interceptor 기능 x)
+// 여기에서 axios 인스턴스를 생성하고,
+// interceptor 기능을 활용하여, access token이 만료되었을 때 refresh token을 사용하여
+// 새로운 access token을 발급받는 비동기 방식의 요청을 모듈화. (fetch는 interceptor 기능 x)
 // axios 인스턴스는 token이 필요한 모든 요청에 활용 될 것입니다.
 
 import axios from 'axios';
-import { API_BASE_URL as BASE, USER } from './host-config';
+import {
+  API_BASE_URL as BASE,
+  TODO,
+  USER,
+} from './host-config';
 import { useNavigate } from 'react-router-dom';
 
-const TODO_URL = BASE;
+const TODO_URL = BASE + TODO;
 const USER_URL = BASE + USER;
 
 // Axios 인스턴스 생성
@@ -19,8 +23,8 @@ const axiosInstance = axios.create({
 });
 
 /*
-  Axios Interceptor는 요청 또는 응답이 처리되기 전에 실행되는 코드입니다.
-  요청을 수정하거나, 응답에 대한 결과 처리를 수행할 수 있습니다.
+Axios Interceptor는 요청 또는 응답이 처리되기 전에 실행되는 코드입니다.
+요청을 수정하거나, 응답에 대한 결과 처리를 수행할 수 있습니다.
 */
 
 // Request Interceptor 설정
@@ -38,28 +42,26 @@ axiosInstance.interceptors.request.use(
 
 // Response Interceptor 설정
 axiosInstance.interceptors.response.use(
-  (response) => response, // 응답에 문제가 없었다며 그대로 리턴
+  (response) => response, // 응답에 문제가 없었다면 그대로 응답 내용 리턴
   async (error) => {
     console.log(
       'response Interceptor가 동작함! 응답 에러 발생!',
     );
     // 원본 요청의 정보를 기억을 해 놓자 -> 새 토큰 받아서 다시 보낼 꺼니까.
-    const originalRequest = error.config; // config를 받아서 위에서 생성해 놓은 기본 설정을 받아온다.
-
-    // const redirection = useNavigate();
+    const originalRequest = error.config;
 
     // 응답에 에러가 발생하면 실행할 두번째 함수.
     if (
       error.response.status === 401 &&
       !originalRequest._retry
     ) {
-      // _retry 사용자 정의 속성 입니다. 최초 요청에서는 조재하지 않습니다.
-      // 만약 재 요청시에도 문제가 발생 했다면 (refresh 만료 등) 더 이상 똑같은 요청을 반복해서 무한 루프에 빠지지 않도록
-      // 막아주는 역할을 한다.
+      console.log('응답 상태 401 확인! 토큰 재발급 요청!');
+      // _retry 속성은 사용자 정의 속성입니다. 최초 요청에서는 존재하지 않습니다.
+      // 만약 재요청 시에도 문제가 발생했다면 (refresh 만료 등), 더 이상 똑같은 요청을 반복해서 무한 루프에 빠지지 않도록
+      // 막아주는 역할을 합니다.
       originalRequest._retry = true;
-      console.log('응답상태 401 확인 토큰 재발급 요청!');
 
-      // 응답상태 401 -> 토큰에 문제가 있음
+      // 응답상태 401? -> 토큰에 문제 있구나!
       try {
         const refreshToken =
           localStorage.getItem('REFRESH_TOKEN');
@@ -67,23 +69,25 @@ axiosInstance.interceptors.response.use(
           `${USER_URL}/refresh`,
           { refreshToken },
         );
-        // ${USER_URL}/refresh 요청이 성공 했다면 서버가 넘겨준 새로운 accessToken을 json에서 꺼내기.
         if (res.status === 200) {
-          const { accessToken } = res.data; // axios는 json()함수 쓰지 않고 data로 json객체 바로 리턴
-          localStorage.setItem('ACCESS_TOKEN', accessToken); // 동일한 이름으로 LocalStorage에 담자.
+          // ${USER_URL}/refresh 요청이 성공했다면 서버가 넘겨준 새로운 accessToken을 json에서 꺼내기.
+          const { accessToken } = res.data; // axios는 json()함수 안씁니다. data라고 하면 json 객체 바로 리턴.
+          console.log('accessToken: ', accessToken);
+
+          localStorage.setItem('ACCESS_TOKEN', accessToken); // 동일한 이름으로 localStorage에 담자.
           // 실패한 원본 요청 정보에서 Authorization의 값을 새 토큰으로 바꿔주자.
           originalRequest.headers.Authorization = `Bearer ${accessToken}`;
           // axios 인스턴스의 기본 header Authorization도 최신 토큰으로 바꿔놓자.
-          axios.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
+          axiosInstance.defaults.headers.common.Authorization = `Bearer ${accessToken}`;
 
-          // axiosInstance를 사용해서 다시 한 번 원보의 요청을 보낼 거고, 응답값을 원래 호출한 곳으로 리턴.
+          // axiosInstance를 사용해서 다시 한 번 원본의 요청을 보낼 거고, 응답값을 원래 호출한 곳으로 리턴.
           return axiosInstance(originalRequest);
         }
+        console.log('뭔가 잘못되었어');
       } catch (err) {
         // Refresh token이 만료된 경우
         localStorage.removeItem('ACCESS_TOKEN');
         localStorage.removeItem('REFRESH_TOKEN');
-        // redirection('/login');
       }
     }
     return Promise.reject(error);
